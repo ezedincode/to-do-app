ef4cddcc25e177dcf46c30db2a0fa997ce80e421
Ezedin Ahmed <ezedinadefa@gmail.com>
2026-02-03 22:28:57 +0300

    fixing some problems

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/componets/front.vue'
 import AddTodo from '@/componets/todoInput.vue'
+import edit from '@/componets/edit.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {path:'/', component:Home},
-    {path:'/AddTodo',component:AddTodo}
+    {path:'/AddTodo',component:AddTodo},
+    {path: '/edit/:id' ,component:edit}
   ],
 })
 
